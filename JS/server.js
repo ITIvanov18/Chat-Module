3e036a3f597025a0d748c6d85c88e84ae52789b1
mvatanasov18 <mvatanasov18@codingburgas.bg>
2021-07-12 09:23:07 +0300

    Install moment.js *Change index.ejs to index.html *Add getRoomUsers and getCurrentUser *Add messages.js

--- JS/server.js
+++ JS/server.js
@@ -1,11 +1,22 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 6969;
 const path = require('path');
-const { joinUser, removeUser } = require('./users');
-app.use(express.static('./'));
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./messages');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const {
+  joinUser,
+  getCurrentUser,
+  removeUser,
+  getRoomUsers
+} = require('./users');
+
+const port = process.env.PORT || 6969;
+
 app.get('/', (req, res) => {
     const index = path.join(__dirname, '/../HTML', '.', '/index.html');
     res.sendFile(index);
@@ -28,6 +39,6 @@ io.on("connection",(socket)=>{
     });
 });
 
-http.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
--- JS/users.js
+++ JS/users.js
@@ -1,21 +1,33 @@
 let users = [];
-function joinUser  (socketId, userName, roomName)
-{
-    const user = 
-    {
-        socketID: socketId,
-        username: userName,
-        roomname: roomName,
-    }
-    users.push(user)
-    return user
+function joinUser(socketId, userName, roomName) {
+  const user =
+  {
+    socketID: socketId,
+    username: userName,
+    roomname: roomName,
+  }
+  users.push(user)
+  return user
+}
+
+function getCurrentUser(id) {
+  return users.find(user => user.id === id);
 }
 
 function removeUser(id) {
-    const getID = users => users.socketID === id;
-   const index =  users.findIndex(getID);
-    if (index !== -1) {
-      return users.splice(index, 1)[0];
-    }
+  const getID = users => users.socketID === id;
+  const index = users.findIndex(getID);
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
   }
-  module.exports ={ joinUser, removeUser}
+}
+
+function getRoomUsers(room) {
+  return users.filter(user => user.room === room);
+}
+module.exports = {
+  joinUser,
+  getCurrentUser,
+  removeUser,
+  getRoomUsers
+};
