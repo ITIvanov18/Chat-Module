38607eccbb7240fd9c7321c36f6e71f9a6ee6703
ITIvanov18 <ITIvanov18@codingburgas.bg>
2021-07-12 10:31:33 +0300

    Create sockets to handle user's input

--- Project/server.js
+++ Project/server.js
@@ -25,21 +25,52 @@ app.get('/', (req, res) => {
 });
 
 var room="";
-io.on("connection",(socket)=>{
-    console.log("connected");
-    socket.on("join",(data)=>{
-        console.log("in room "+data.roomName);
-        let newUser = joinUser(socket.id,data.username,data.roomName);
-        socket.emit("IUR socket", {
-            id:socket.id,
-            username:newUser.username,
-            roomName: newUser.roomname
-        });
-        room = newUser.roomname;
-        console.log(newUser);
-        socket.join(newUser.roomname);
+io.on('connection', socket => {
+    socket.on('joinRoom', ({ username, room }) => {
+      const user = userJoin(socket.id, username, room);
+   
+      socket.join(user.room);
+   
+      // Broadcast when a user connects
+      socket.broadcast
+        .to(user.room)
+        .emit(
+          'message',
+          formatMessage(' ', '${user.username} has joined the chat')
+        );
+   
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
     });
-});
+   
+    // Listen for chatMessage
+    socket.on('chatMessage', msg => {
+      const user = getCurrentUser(socket.id);
+   
+      io.to(user.room).emit('message', formatMessage(user.username, msg));
+    });
+   
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+      const user = userLeave(socket.id);
+   
+      if (user) {
+        io.to(user.room).emit(
+          'message',
+          formatMessage(' ', '${user.username} has left the chat')
+        );
+   
+        // Send users and room info
+        io.to(user.room).emit('roomUsers', {
+          room: user.room,
+          users: getRoomUsers(user.room)
+        });
+      }
+    });
+  });
 
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
